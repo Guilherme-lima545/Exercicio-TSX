2044761996ef78d557259344d2fc95d11f78468d
Guilherme <guisupergames123@gmail.com>
2025-06-02 20:57:32 -0300

    update para o deploy

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,12 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+   base: '/Exercicio-TSX',
+  build: {
+    rollupOptions: {
+      output: {
+        assetFileNames: 'assets/[name].[ext]',
+      },
+    },
+  },
 })
